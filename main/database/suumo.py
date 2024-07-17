faf575f8688a59fad81851cc5df7a5809cd04045
kazukingh01 <kazukingh01@gmail.com>
2024-07-18 05:45:08 +0900

    modify

--- main/database/suumo.py
+++ main/database/suumo.py
@@ -90,7 +90,7 @@ def get_estate_detail(url):
     assert isinstance(url, str)
     url  = f"{url}property/" if url[-1] == "/" else f"{url}/property/"
     LOGGER.info(url)
-    html = requests.get(url, timeout=3)
+    html = requests.get(url, timeout=10)
     if html.status_code in [503]:
         LOGGER.warning(f"STATUS CODE: {html.status_code}")
         return -1
